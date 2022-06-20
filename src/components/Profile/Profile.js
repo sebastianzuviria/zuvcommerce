e67b3eaadb5988b9e653571f8ff8afea42fb140e
Sebastian Zuviria <szuviria@gmail.com>
2022-06-20 03:55:13 -0300

    Changed label

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -103,7 +103,7 @@ const Profile = () => {
                     color='#ffffff'
                     onClick={signout}
                 >
-                    Logout
+                    Sign out
                 </Button>
             </Flex>
         </Flex>
